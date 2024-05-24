26e19ee0591bc792de8ec866e831b5d942222177
Karim Ahmed Mahmoud Ahmed <kareemahmed66@outlook.com>
2024-05-24 19:14:24 +0300

    fix config

--- server.ts
+++ server.ts
@@ -1,16 +1,19 @@
+import 'colors';
 import dotenv from 'dotenv';
 import express from 'express';
 import cors from 'cors';
 import morgan from 'morgan';
 import cookieSession from 'cookie-session';
-dotenv.config();
+
+import db_connection from 'config/db_connection';
+
+dotenv.config({ path: 'config/config.env' });
 
 const PORT = process.env.PORT || 3001;
 const COOKIE_MAX_AGE = 2 * 24 * 60 * 60 * 1000; //  2 * 24 hours = 2 days
 
 import { globalErrorMiddleware, globalNotFoundMiddleware } from '@/middlewares';
 import { mountRouter } from '@/routers';
-//import { rateLimitMiddleware } from '@/middlewares';
 
 const app = express();
 
@@ -34,12 +37,12 @@ app.use(
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
-//app.use(rateLimitMiddleware); //  apply to all requests
 app.use('/api/v1', mountRouter);
 
 app.all('*', globalNotFoundMiddleware);
 app.use(globalErrorMiddleware);
 
 app.listen(PORT, () => {
-  console.log(`Server listening on port ${PORT}`);
+  db_connection();
+  console.log(`Server listening on port ${PORT}`.cyan.bold);
 });
